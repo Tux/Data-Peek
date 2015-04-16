3e8f11113da11e82707dd2d5acf6f12efaa534e8
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-04-16 17:33:25 +0200

    Skip changelog test in parent check

--- Peek.pm
+++ Peek.pm
@@ -6,7 +6,7 @@ use warnings;
 use DynaLoader ();
 
 use vars qw( $VERSION @ISA @EXPORT @EXPORT_OK );
-$VERSION   = "0.44";
+$VERSION   = "0.45";
 @ISA       = qw( DynaLoader Exporter );
 @EXPORT    = qw( DDumper DTidy DDsort DPeek DDisplay DDump DHexDump
 		 DDual DGrow );
