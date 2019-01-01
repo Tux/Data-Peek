bc1a57fd07b6390a632823b728a84a291bb7d872
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2019-01-01 17:29:50 +0100

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -59,10 +59,10 @@ test_requires:
     Test::More:          0.88
     Test::NoWarnings:    0
 recommends:
-    Data::Dumper:        2.172
+    Data::Dumper:        2.173
     Perl::Tidy:          0
 test_recommends:
-    Test::More:          1.302140
+    Test::More:          1.302141
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/Data-Peek
