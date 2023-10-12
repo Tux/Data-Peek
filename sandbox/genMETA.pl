6e91645ec1e9129bab9bbe225308f5b23c81ae6c
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2023-10-12 10:52:10 +0200

    Update doc for groff-1.24

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -41,7 +41,7 @@ version:                 VERSION
 abstract:                Modified and extended debugging facilities
 license:                 perl
 author:
-    - H.Merijn Brand <hmbrand.org>
+    - H.Merijn Brand <hmbrand@cpan.org>
 generated_by:            Author
 distribution_type:       module
 provides:
